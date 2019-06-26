c6a29178c91168b1f5ffcc7c6f4908ca35ccdb5c
jean <jpaltahonacc@gmail.com>
2019-06-26 13:51:26 -0500

    instalando axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component{
     constructor(){
      super();
      
      this.state = {
-        usuarios: [
-            {
-                nombre: 'rodolfo',
-                correo: 'rodolfo@gmail.com',
-                enlace: 'rodolfo.com'
-            },
-            {
-                nombre: 'platzi',
-                correo: 'platzi@gmail.com',
-                enlace: 'platzi.com'
-            },
-            ]
+        usuarios: []
         }
+    }
+    async componentDidMount(){
+      const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+      console.log('respuesta:', respuesta);
+      this.setState({
+        usuarios: respuesta.data
+      }) 
     }
 ponerfilas = () => (
         this.state.usuarios.map((usuario, index)=> (
-            <tr key={index}>
-                <td>{usuario.nombre}</td>
-                <td>{usuario.correo}</td>
-                <td>{usuario.enlace}</td>
+            <tr key={usuario.id}>
+                <td>{usuario.name}</td>
+                <td>{usuario.email}</td>
+                <td>{usuario.website}</td>
             </tr>
         ))
     );
 render(){
+  console.log(this.state.usuarios);
 return(
     <div className="margen">
       <table className="tabla">
