72638633c008eb70da5d4821cccf75f3b53c44eb
jean <jpaltahonacc@gmail.com>
2019-06-28 14:06:21 -0500

    usando redux de forma global

--- src/actions/usuariosActions.js
+++ src/actions/usuariosActions.js
@@ -1,6 +1,22 @@
-export const traertodos = () => (dispatch) => {
+import axios from 'axios';
+import { TRAER_TODOS, CARGANDO, ERROR } from '../types/usuariosTypes';
+
+export const traertodos = () => async(dispatch) => {
 	dispatch({
-		type: 'traer_usuarios',
-		payload: [1, 2, 3]
+		type : CARGANDO,
 	});
+	try {
+		const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
+		dispatch({
+			type: TRAER_TODOS ,
+			payload: respuesta.data
+		});
+	}
+	catch(error){
+		console.log(error.message);
+		dispatch({
+			type: ERROR ,
+			payload: error.message
+		});
+	}
 };
--- src/components/users/index.js
+++ src/components/users/index.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { connect } from 'react-redux';
 import * as usuariosActions from '../../actions/usuariosActions';
 
 class Users extends Component{
   componentDidMount(){
-    /*   const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users');
-      console.log('respuesta:', respuesta);
-      this.setState({
-        usuarios: respuesta.data
-      }) */
       this.props.traertodos();
     } 
 ponerfilas = () => (
@@ -21,25 +15,24 @@ ponerfilas = () => (
             </tr>
         ))
     );
-render(){
-  console.log(this.props);
-return(
-    <div>
-      <table className="tabla">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>corre</th>
-          <th>Enlace</th>
-        </tr>
-      </thead>
-      <tbody>
-        { this.ponerfilas() }
-      </tbody>
-    </table>
-    </div>
-  );
-}
+    render(){
+    return(
+        <div>
+            <table className="tabla">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>corre</th>
+                <th>Enlace</th>
+            </tr>
+            </thead>
+            <tbody>
+            { this.ponerfilas() }
+            </tbody>
+        </table>
+        </div>
+        );
+    }
 }
 
 const mapStateToProps = (reducers) => {
